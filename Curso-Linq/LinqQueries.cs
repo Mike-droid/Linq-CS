c35ee8585c8960e8e041536eab108a5e2fbf3c84
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-05-22 22:45:03 -0500

    Clase 8

--- Curso-Linq/LinqQueries.cs
+++ Curso-Linq/LinqQueries.cs
@@ -8,6 +8,19 @@ namespace Curso_Linq
 {
     internal class LinqQueries
     {
-        public LinqQueries() { }
+        private List<Book> librosCollection = new List<Book>();
+        public LinqQueries()
+        {
+            using (StreamReader reader = new StreamReader("books.json"))
+            {
+                string json = reader.ReadToEnd();
+                this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            }
+        }
+
+        public IEnumerable<Book> TodaLaColeccion()
+        {
+            return librosCollection;
+        }
     }
 }
--- Curso-Linq/Program.cs
+++ Curso-Linq/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Curso_Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        LinqQueries queries = new LinqQueries();
+
+        string formatoTexto = "{0, -60} {1, 15} {2, 15}";
+
+        void ImprimirValores(IEnumerable<Book> listaDeLibros)
+        {
+            Console.WriteLine($"{formatoTexto}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+            foreach (var book in listaDeLibros)
+            {
+                Console.WriteLine($"{formatoTexto}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+            }
+        }
+
+        ImprimirValores(queries.TodaLaColeccion());
+    }
+}
